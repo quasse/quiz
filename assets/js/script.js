fba7ea4c8518b562e18fe219f682fac74cc282e4
Sam Morgen <samorgen14@gmail.com>
2021-05-21 20:45:05 -0700

    Set up initial structure

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var timerEl = document.getElementById("timer");
+var startButtonEl = document.getElementById("start-quiz");
+
+var timer = function () {
+  var timeLeft = 60;
+  var timeInterval = setInterval(function () {
+    var countdown = timerEl;
+    if (timeLeft >= 1) {
+      countdown.textContent = "Time left: " + timeLeft;
+      timeLeft--;
+    } else {
+      countdown.textContent = "Out of time";
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+};
+
+var displayMessage = function () {
+  timerEl.append("Hello");
+};
+
+startButtonEl.addEventListener("click", timer);
